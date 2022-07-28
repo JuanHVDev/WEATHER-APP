e01b158ff59f8e68bbb47b303e652e4ad6c10c2e
Juan <jherrerav1901@alumno.ipn.mx>
2022-07-28 00:17:44 -0500

    Se agrego la funcion para obtener informacion del input y obtener la informacion del clima

---
+++ src/components/formulario.js
@@ -0,0 +1,17 @@
+const Formulario = () => {
+    const formulario = document.createElement('form');
+    const textBusqueda = document.createElement('label');
+    textBusqueda.setAttribute('for', 'text-busqueda');
+    textBusqueda.classList.add('text-busqueda');
+    textBusqueda.textContent = "Buscar:"
+    const busqueda = document.createElement('input');
+    busqueda.setAttribute('type', 'text');
+    busqueda.classList.add('input-busqueda')
+    const buttonBuscar = document.createElement('button');
+    buttonBuscar.textContent = "Buscar"
+    buttonBuscar.classList.add('button-buscar')
+    formulario.append(textBusqueda, busqueda, buttonBuscar)
+    return formulario;
+}
+
+export default Formulario;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import './styles.css';
 import Title from './components/portada';
+import Formulario from './components/formulario';
 const divPrincipal = document.querySelector('.root')
 
-divPrincipal.append(Title())
+divPrincipal.append(Title(), Formulario())
+
+// fetch(url).then(res=>res.json()).then(({name, main:{temp}, sys:{country}})=>console.log(name, temp, country))
+
+const formulario = document.querySelector('form')
+
+window.addEventListener('load', ()=>{
+    formulario.addEventListener('submit', buscarClima)
+})
+
+function buscarClima(e) {
+    e.preventDefault();
+    const ciudad = document.querySelector('.input-busqueda').value
+    console.log(ciudad)
+    if(ciudad === ''){
+        alert('Ingresas la ciudad a buscar')
+        return;
+    }
+    consultarAPI(ciudad)
+}
+
+function consultarAPI(ciudad){
+    const API = '8bab29329a4074990e74a49b50f9c57f'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&lang=sp&units=metric&appid=${API}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+    })
+}
